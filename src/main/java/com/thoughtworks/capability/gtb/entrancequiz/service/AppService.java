b894bc862b5af58c09099a3eae8da7ae3dfe19c7
Lu <1625196521@qq.com>
2020-10-27 20:26:54 +0800

    get studentList

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/AppService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/AppService.java
@@ -1,4 +1,33 @@
 package com.thoughtworks.capability.gtb.entrancequiz.service;
 
+import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AppService {
+    private List<Student> studentList;
+
+    public AppService() {
+        studentList.add(new Student(1, "成吉思汗"));
+        studentList.add(new Student(2, "鲁班七号"));
+        studentList.add(new Student(3, "太乙真人"));
+        studentList.add(new Student(4, "钟无艳"));
+        studentList.add(new Student(5, "花木兰"));
+        studentList.add(new Student(6, "雅典娜"));
+        studentList.add(new Student(7, "芈月"));
+        studentList.add(new Student(8, "白起"));
+        studentList.add(new Student(9, "刘禅"));
+        studentList.add(new Student(10, "庄周"));
+        studentList.add(new Student(11, "马超"));
+        studentList.add(new Student(12, "刘备"));
+        studentList.add(new Student(13, "哪吒"));
+        studentList.add(new Student(14, "大乔"));
+        studentList.add(new Student(15, "蔡文姬"));
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
 }
